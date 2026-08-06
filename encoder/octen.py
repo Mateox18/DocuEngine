4bae3cc6e658c85d9a29395c1e8f1810464f2596
abetancur2001 <a.betancurc@uniandes.edu.co>
2026-08-06 06:47:13 -0500

    avances

---
+++ encoder/octen.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+if torch.backends.mps.is_available():
+    DEVICE = 'mps'
+else:
+    DEVICE = 'cpu'
+
+print(f"Usando device: {DEVICE}")
+
+MODEL_NAME = "Octen/Octen-Embedding-8B-INT8"
+
+model = SentenceTransformer(MODEL_NAME, device=DEVICE)
+
+print("Modelo cargado correctamente.")
